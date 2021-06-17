9d9554e7dc05cba3658b91b3e49d1ba7e31f232e
Kanvaly Fadiga <kfadiga@tp-5d02-04.enst.fr>
2021-06-17 12:46:07 +0200

    update requirements

--- train_vxm_mnist.py
+++ train_vxm_mnist.py
@@ -7,7 +7,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--save', action='store_true', help="save the model")
 parser.add_argument('--folder', default='output/', type=str, help="save folder")
-parser.add_argument('--name', default='lambda-0_5', type=str, help="the configuration for training")
+parser.add_argument('--name', default='default', type=str, help="the configuration for training")
 parser.add_argument('--verbose', action='store_true', help="verbose")
 args = parser.parse_args()
 
